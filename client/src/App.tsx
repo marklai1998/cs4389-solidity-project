d3358f66af28d26d4eefba469dd8d11ef9839814
Mark Lai <marklai@hk01.com>
2020-04-18 23:23:09 +0800

    feat: use type script

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,59 +1,70 @@
-import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
-import getWeb3 from "./getWeb3";
+import React from 'react'
+import SimpleStorageContract from './contracts/SimpleStorage.json'
+import getWeb3 from './getWeb3'
+import { AbiItem } from 'web3-utils'
+import { Contract } from 'web3-eth-contract'
 
-import "./App.css";
+import './App.css'
+import Web3 from 'web3'
 
-class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+type State = {
+  storageValue: number
+  web3: Web3 | null
+  accounts: string[] | null
+  contract: Contract | null
+}
+
+class App extends React.PureComponent<{}, State> {
+  state: State = { storageValue: 0, web3: null, accounts: null, contract: null }
 
   componentDidMount = async () => {
     try {
       // Get network provider and web3 instance.
-      const web3 = await getWeb3();
+      const web3 = await getWeb3()
 
       // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
+      const accounts = await web3.eth.getAccounts()
 
       // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
+      const networkId = await web3.eth.net.getId()
+      const deployedNetwork = (SimpleStorageContract as any).networks[networkId]
       const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
+        SimpleStorageContract.abi as AbiItem[],
         deployedNetwork && deployedNetwork.address
-      );
+      )
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      this.setState({ web3, accounts, contract: instance }, this.runExample)
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
         `Failed to load web3, accounts, or contract. Check console for details.`
-      );
-      console.error(error);
+      )
+      console.error(error)
     }
-  };
+  }
 
   runExample = async () => {
-    const { accounts, contract } = this.state;
+    const { accounts, contract } = this.state
+    if (!accounts || !contract) return
 
     // Stores a given value, 5 by default.
-    await contract.methods.set(10).send({ from: accounts[0] });
+    await contract.methods.set(5).send({ from: accounts[0] })
 
     // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
+    const response = await contract.methods.get().call()
 
     // Update state with the result.
-    this.setState({ storageValue: response });
-  };
+    this.setState({ storageValue: response })
+  }
 
   render() {
     if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
+      return <div>Loading Web3, accounts, and contract...</div>
     }
     return (
-      <div className="App">
+      <div className='pp'>
         <h1>Good to Go!</h1>
         <p>Your Truffle Box is installed and ready.</p>
         <h2>Smart Contract Example</h2>
@@ -66,8 +77,8 @@ class App extends Component {
         </p>
         <div>The stored value is: {this.state.storageValue}</div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
